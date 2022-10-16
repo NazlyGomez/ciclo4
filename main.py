4b3500e169b5da80e043b32fd6b36d9e85be5248
sebas3536 <delahozpablo005@gmail.com>
2022-10-16 12:32:14 -0500

    se monta un nuevo archivo

--- main.py
+++ main.py
@@ -5,9 +5,22 @@ from flask_cors import CORS
 import json
 from waitress import serve
 
+from Controladores.ControladorEstudiante import ControladorEstudiante
+
 app=Flask(__name__)
 cors = CORS(app)
 
+controladorEstudiante = ControladorEstudiante()
+@app.route("/estudiante",methods=['POST'])
+def crearEstudiante():
+    requestBody = request.get_json()
+    print("Este es el objeto que recibo en python ", requestBody)
+    resultado = controladorEstudiante.create(requestBody)
+    if resultado:
+        return{"resultado": "Estudiante creado con exito"}
+    else:
+        return {"resultado": "Error al crear el estudiante"}
+    return{"Resultado" : "EstudianteCreado"}
 
 @app.route("/TEST-947/<string:Cedula>",methods=['GET'])
 def test(Cedula):
